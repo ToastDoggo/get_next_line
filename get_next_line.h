b2197f5b764098c3e4c61cca8c616914809184ce
Shasla <rehernan@students.42woflsburg.de>
2022-02-07 15:31:19 +0100

    Final_version

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rehernan <rehernan@students.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/06 21:31:43 by rehernan          #+#    #+#             */
+/*   Updated: 2022/01/12 21:27:21 by rehernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*ft_strjoin(char *s1, char const *s2);
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+
+#endif
